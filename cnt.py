4b9f6b042481dd4447c53c5de5a1ec679b50d60e
Joshua Morse <jwmorse@unm.edu>
2019-11-03 18:57:26 -0700

    added function to combine multiple csv files

--- cnt.py
+++ cnt.py
@@ -1,7 +1,34 @@
-import pandas as pd
-import numpy as np
-df = pd.read_csv('EtsySoldOrderItems2018.csv')
 
-df['var_cl'] = df['Variations'].str.split('US').str[0]
-df['var_cl'] = df['var_cl'].str.split(':').str[1]
-print(df['var_cl'].value_counts())
+# File: cnt.py
+# Using Python 3.4.2
+
+# This program combines .csv files into a single frame, then
+# takes some data from a specific column, cleans it up, and
+# prints the number off occurences of an item in that column. 
+
+# data is in the origional format: 
+# Ring size:9.25 US,Width:6.5mm
+
+# This function alters the string such that the data is in 
+# the new format of:
+# 9.25
+
+
+import pandas as pd
+import glob
+
+path = r'/home/parallels/Documents/prog/etsy/ringSizeCount/data' #location where .csv are stored
+allFiles = glob.glob(path + "/*.csv")
+
+list = []
+
+for filename in allFiles:
+	df = pd.read_csv(filename, index_col=None, header=0)
+	list.append(df)
+
+frame = pd.concat(list, axis=0, ignore_index=True)
+
+
+frame['var_cl'] = frame['Variations'].str.split('US').str[0] 
+frame['var_cl'] = frame['var_cl'].str.split(':').str[1]
+print(frame['var_cl'].value_counts())
